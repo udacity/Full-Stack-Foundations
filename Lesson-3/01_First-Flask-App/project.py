eeea2596037e57717e3bfd1f48f7b575550a1e4f
Lorenzo Brown <lorenzo@udacity.com>
2015-02-04 14:32:11 -0800

    Update project.py

--- Lesson-3/01_First-Flask-App/project.py
+++ Lesson-3/01_First-Flask-App/project.py
@@ -1,1 +1,11 @@
-#
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/hello')
+def HelloWorld():
+    return "Hello World"
+    
+if __name__ == '__main__':
+  app.debug = True
+  app.run(host = '0.0.0.0', port = 5000)
