b4bc4106a475eb654300043864f2524515419735
Lorenzo Brown <lorenzo@udacity.com>
2015-02-04 15:06:30 -0800

    Update project.py

--- Lesson-3/04_Routing/project.py
+++ Lesson-3/04_Routing/project.py
@@ -1,1 +1,32 @@
-#
+from flask import Flask
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def HelloWorld():
+  restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+  items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+  output = ''
+  for i in items:
+    output += i.name
+    output += '</br>'
+    output += i.price
+    output += '</br>'
+    output += i.description
+    output +='</br>'
+    output +='</br>'
+  return output
+  
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host = '0.0.0.0', port = 5000)
