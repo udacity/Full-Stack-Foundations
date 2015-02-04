d4ef6030188bf094fe1bdf83ba07ebdd34df78d0
Lorenzo Brown <lorenzo@udacity.com>
2015-02-04 11:39:20 -0800

    Update webserver.py

--- Lesson-2/Objective-3-Solution/webserver.py
+++ Lesson-2/Objective-3-Solution/webserver.py
@@ -1,1 +1,90 @@
-#
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
+
+## import CRUD Operations from Lesson 1 ##
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+##Create session and connect to DB ##
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+
+class webServerHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        try:
+            #Objective 3 Step 2 - Create /restarants/new page
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Make a New Restaurant</h1>"
+                output += "<form method = 'POST' enctype='multipart/form-data' action = '/restaurants/new'>"
+                output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant Name' > "
+                output += "</form></html></body>"
+                self.wfile.write(output)
+                return
+              
+            if self.path.endswith("/restaurants"):
+                restaurants = session.query(Restaurant).all()
+                output = ""
+                #Objective 3 Step 1 - Create a Link to create a new menu item
+                output += "<a href = '/restaurants/new' > Make a New Restaurant Here </a></br></br>
+                
+                self.send_response(200)
+                self.send_header('Content-type','text/html')
+                self.end_headers()
+                output += "<html><body>"
+                for restaurant in restaurants:
+                    output += restaurant.name
+                    output += "</br>"
+                    ##Objective 2 -- Add Edit and Delete Links
+                    output += "<a href ='#' >Edit </a> " 
+                    output += "</br>"
+                    output += "<a href =' #'> Delete </a>"
+                    output += "</br></br></br>"
+                    
+                output += "</body></html>"
+                self.wfile.write(output)
+                return
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+    #Objective 3 Step 3- Make POST method
+    def do_POST(self):
+      try:
+        if self.path.endswith("/restaurants/new"):
+          if self.path.endswith("/restaurants/new"):
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            if ctype == 'mulitpart/form-data'"
+              fields = cgi.parse_multipart(self.rfile,pdict)
+              messagecontent = fields.get('newRestaurantName')
+              
+              #Create new Restaurant Object
+              newRestaurant = Restaurant(name = messagecontent[0])
+              session.add(newRestaurant)
+              session.commit()
+              
+              self.send_response(301)
+              self.send_header('Content-type', 'text/html')
+              self.send_header('Location','/restaurants')
+              self.end_headers()
+     
+
+def main():
+    try:
+        server = HTTPServer(('', 8080), webServerHandler)
+        print 'Web server running...open localhost:8080/restaurants in your browser'
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print '^C received, shutting down server'
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
