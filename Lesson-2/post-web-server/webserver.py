dc183487cefb68857c2ab2e9347f0252baf84e6c
Lorenzo Brown <lorenzo@udacity.com>
2015-02-04 11:08:02 -0800

    Update webserver.py
    port 8080

--- Lesson-2/post-web-server/webserver.py
+++ Lesson-2/post-web-server/webserver.py
@@ -57,7 +57,7 @@ def do_POST(self):
 
 def main():
 	try:
-		port = 80
+		port = 8080
 		server = HTTPServer(('', port), webServerHandler)
 		print "Web Server running on port %s"  % port
 		server.serve_forever()
