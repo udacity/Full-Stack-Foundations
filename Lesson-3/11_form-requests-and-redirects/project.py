6afcefecfc1f6bb31d0bff6ca8ebda9ba05e7b68
Lorenzo Brown <lorenzo@udacity.com>
2015-02-04 16:17:02 -0800

    Update project.py

--- Lesson-3/11_form-requests-and-redirects/project.py
+++ Lesson-3/11_form-requests-and-redirects/project.py
@@ -1,1 +1,39 @@
-#
+from flask import Flask, render_template
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def HelloWorld():
+  restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+  items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+  return render_template('menu.html', restaurant=restaurant, items = items)
+  
+#Task 1: Create route for newMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET','POST'])
+def newMenuItem(restaurant_id):
+  return "page to create a new menu item. Task 1 complete!"
+  
+#Task 2: Create route for editMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id/edit/')
+def editMenuItem(restaurant_id, menu_id)
+  return "page to edit a menu item. Task 2 complete!"
+  
+#Task 3: Create a route for deleteMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+  return "page to delete a menu item. Task 3 complete!"
+  
+  
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host = '0.0.0.0', port = 5000)
