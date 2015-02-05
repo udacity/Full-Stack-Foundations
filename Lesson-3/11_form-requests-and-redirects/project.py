881ab7d1be0353a608194e49a46555386a95ce14
Lorenzo Brown <lorenzo@udacity.com>
2015-02-04 16:32:03 -0800

    Update project.py

--- Lesson-3/11_form-requests-and-redirects/project.py
+++ Lesson-3/11_form-requests-and-redirects/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -21,7 +21,13 @@ def HelloWorld():
 #Task 1: Create route for newMenuItem function here
 @app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET','POST'])
 def newMenuItem(restaurant_id):
-  return "page to create a new menu item. Task 1 complete!"
+  if request.method == 'POST':
+  	newItem = MenuItem(name = request.form['name'],restaurant_id=restaurant_id)
+  	session.add(newItem)
+  	session.commit()
+  	return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+  else:
+  	return render_template('newmenuitem.html',restaurant_id = restaurant_id)
   
 #Task 2: Create route for editMenuItem function here
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id/edit/')
