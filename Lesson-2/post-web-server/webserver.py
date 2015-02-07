deea432080ae45c22a49ffdf4cb7dcecbd02a0d1
Wenchao Hu <zjuhwc@gmail.com>
2015-02-07 19:17:45 +0800

    Update webserver.py

--- Lesson-2/post-web-server/webserver.py
+++ Lesson-2/post-web-server/webserver.py
@@ -35,24 +35,24 @@ class webServerHandler(BaseHTTPRequestHandler):
 			self.send_error(404, 'File Not Found: %s' % self.path)
 
 
-def do_POST(self):
-		try:
-			self.send_response(301)
-			self.end_headers()
-			ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-			if ctype == 'multipart/form-data':
-				fields=cgi.parse_multipart(self.rfile, pdict)
-				messagecontent = fields.get('message')
-			output = ""
-			output +=  "<html><body>"
-			output += " <h2> Okay, how about this: </h2>"
-			output += "<h1> %s </h1>" % messagecontent[0]
-			output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-			output += "</html></body>"
-			self.wfile.write(output)
-			print output
-		except:
-			pass
+	def do_POST(self):
+			try:
+				self.send_response(301)
+				self.end_headers()
+				ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+				if ctype == 'multipart/form-data':
+					fields=cgi.parse_multipart(self.rfile, pdict)
+					messagecontent = fields.get('message')
+				output = ""
+				output +=  "<html><body>"
+				output += " <h2> Okay, how about this: </h2>"
+				output += "<h1> %s </h1>" % messagecontent[0]
+				output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+				output += "</html></body>"
+				self.wfile.write(output)
+				print output
+			except:
+				pass
 
 
 def main():
