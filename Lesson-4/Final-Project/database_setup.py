48d59127b33a7f5b4cdffdcf33cabd3a139657c1
Lorenzo Brown <lorenzo@udacity.com>
2015-02-09 17:21:44 -0800

    Update database_setup.py
    bug fixes

--- Lesson-4/Final-Project/database_setup.py
+++ Lesson-4/Final-Project/database_setup.py
@@ -12,6 +12,14 @@ class Restaurant(Base):
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'id'         : self.id,
+       }
  
 class MenuItem(Base):
     __tablename__ = 'menu_item'
