d58f4efd7c5a55f4bc962061526bf09d62ccb91b
Lorenzo Brown <lorenzo@udacity.com>
2015-02-12 14:33:30 -0800

    Create project.py

---
+++ Lesson-3/05_Routing-Create-Quiz-Template-Code/project.py
@@ -0,0 +1,45 @@
+from flask import Flask
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output +='</br>'
+        output +='</br>'
+    return output
+
+#Task 1: Create route for newMenuItem function here
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+#Task 2: Create route for editMenuItem function here
+def editMenuItem(restaurant_id, menu_id)
+    return "page to edit a menu item. Task 2 complete!"
+
+#Task 3: Create a route for deleteMenuItem function here
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
+
