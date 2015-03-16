f2a10ffff694c0709e2ec207b3187f8031a80790
Hassaan Bin Aslam <hassaanbinaslam@gmail.com>
2015-03-17 01:58:56 +0500

    Update webserver.py
    header('Content-type', 'text/html') added in POST response.

--- Lesson-2/post-web-server/webserver.py
+++ Lesson-2/post-web-server/webserver.py
@@ -38,6 +38,7 @@ class webServerHandler(BaseHTTPRequestHandler):
 	def do_POST(self):
 		try:
 			self.send_response(301)
+			self.send_header('Content-type', 'text/html')
 			self.end_headers()
 			ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
 			if ctype == 'multipart/form-data':
