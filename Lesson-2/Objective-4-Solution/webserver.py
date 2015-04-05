42cfa4c315e51cd6f5c981b5f86ec052c95eb688
Sasha Varlamov <sasha@sashavarlamov.com>
2015-04-05 09:48:15 -0400

    Replaced The Rest of the 'mulitpart' Errors
    There were more instances of this misspelling, between both objective 4
    and 5 in lesson 2, but all instances have no been fixed to 'multipart'

--- Lesson-2/Objective-4-Solution/webserver.py
+++ Lesson-2/Objective-4-Solution/webserver.py
@@ -101,7 +101,7 @@ class webServerHandler(BaseHTTPRequestHandler):
             if self.path.endswith("/restaurants/new"):
                 ctype, pdict = cgi.parse_header(
                     self.headers.getheader('content-type'))
-                if ctype == 'mulitpart/form-data':
+                if ctype == 'multipart/form-data':
                     fields = cgi.parse_multipart(self.rfile, pdict)
                     messagecontent = fields.get('newRestaurantName')
 
