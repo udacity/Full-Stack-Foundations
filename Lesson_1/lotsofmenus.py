f6bb58d8687a02705d7019673d4231a95e50cc37
Miles Erickson <miles.erickson@gmail.com>
2015-09-21 16:09:20 -0700

    Remove duplicate 'Veggie Burger' item from 'Urban Burger' restaurant menu

--- Lesson_1/lotsofmenus.py
+++ Lesson_1/lotsofmenus.py
@@ -25,12 +25,6 @@ restaurant1 = Restaurant(name="Urban Burger")
 session.add(restaurant1)
 session.commit()
 
-menuItem2 = MenuItem(name="Veggie Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
-                     price="$7.50", course="Entree", restaurant=restaurant1)
-
-session.add(menuItem2)
-session.commit()
-
 
 menuItem1 = MenuItem(name="French Fries", description="with garlic and parmesan",
                      price="$2.99", course="Appetizer", restaurant=restaurant1)
