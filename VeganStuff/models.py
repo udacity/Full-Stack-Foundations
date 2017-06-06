360cf2fc84e47bba682219208ec03eeda9ff7a8f
Mathias Wøbbe <allmynameswastaken@gmail.com>
2017-06-06 23:56:44 +0200

    Flask application, still ony routes to all the pages need for the app. Link to mock-up is found here: https://xd.adobe.com/view/10aed435-b28f-4924-a684-bcd0f5b91d76
    This time all the files are comitted, nothing really interesting by any means

---
+++ VeganStuff/models.py
@@ -0,0 +1,7 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+# TODO: Create some database magic using sqlalchemy!
