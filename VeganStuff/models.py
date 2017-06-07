6acc960be58f4732be4993093318a56e3f3369d9
Mathias Wøbbe <allmynameswastaken@gmail.com>
2017-06-07 22:20:11 +0200

    Added DB models, everything should work as intended at this point. Routes are working and so are id's.
    Made new dummy HTML to check if results are as expected.

--- VeganStuff/models.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-
-Base = declarative_base()
-# TODO: Create some database magic using sqlalchemy!
