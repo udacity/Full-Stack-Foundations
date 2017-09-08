4587edd2d37036912fcc3c6ef16131aebbf97b3a
tingtingting <matthewting@users.noreply.github.com>
2017-09-08 03:52:38 -0700

    Update project.py
    must import redirect in order to use it.

--- Lesson-3/12_Edit-Menu-Form/project.py
+++ Lesson-3/12_Edit-Menu-Form/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
