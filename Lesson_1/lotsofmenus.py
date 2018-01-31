c658dc1afa213c9d06451b8116b7782b33b35422
hadeermoustafa <hadeer.moustafa@outlook.com>
2018-01-31 23:21:13 +0200

    Update lotsofmenus.py

--- Lesson_1/lotsofmenus.py
+++ Lesson_1/lotsofmenus.py
@@ -25,10 +25,10 @@ restaurant1 = Restaurant(name="Urban Burger")
 session.add(restaurant1)
 session.commit()
 
-menuItem2 = MenuItem(name="Veggie Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
+menuItem = MenuItem(name="Veggie Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
                      price="$7.50", course="Entree", restaurant=restaurant1)
 
-session.add(menuItem2)
+session.add(menuItem)
 session.commit()
 
 
@@ -126,254 +126,254 @@ session.commit()
 
 
 # Menu for Panda Garden
-restaurant1 = Restaurant(name="Panda Garden")
+restaurant3 = Restaurant(name="Panda Garden")
 
-session.add(restaurant1)
+session.add(restaurant3)
 session.commit()
 
 
 menuItem1 = MenuItem(name="Pho", description="a Vietnamese noodle soup consisting of broth, linguine-shaped rice noodles called banh pho, a few herbs, and meat.",
-                     price="$8.99", course="Entree", restaurant=restaurant1)
+                     price="$8.99", course="Entree", restaurant=restaurant3)
 
 session.add(menuItem1)
 session.commit()
 
 menuItem2 = MenuItem(name="Chinese Dumplings", description="a common Chinese dumpling which generally consists of minced meat and finely chopped vegetables wrapped into a piece of dough skin. The skin can be either thin and elastic or thicker.",
-                     price="$6.99", course="Appetizer", restaurant=restaurant1)
+                     price="$6.99", course="Appetizer", restaurant=restaurant3)
 
 session.add(menuItem2)
 session.commit()
 
 menuItem3 = MenuItem(name="Gyoza", description="The most prominent differences between Japanese-style gyoza and Chinese-style jiaozi are the rich garlic flavor, which is less noticeable in the Chinese version, the light seasoning of Japanese gyoza with salt and soy sauce, and the fact that gyoza wrappers are much thinner",
-                     price="$9.95", course="Entree", restaurant=restaurant1)
+                     price="$9.95", course="Entree", restaurant=restaurant3)
 
 session.add(menuItem3)
 session.commit()
 
 menuItem4 = MenuItem(name="Stinky Tofu", description="Taiwanese dish, deep fried fermented tofu served with pickled cabbage.",
-                     price="$6.99", course="Entree", restaurant=restaurant1)
+                     price="$6.99", course="Entree", restaurant=restaurant3)
 
 session.add(menuItem4)
 session.commit()
 
-menuItem2 = MenuItem(name="Veggie Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
-                     price="$9.50", course="Entree", restaurant=restaurant1)
+menuItem5 = MenuItem(name="Veggie Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
+                     price="$9.50", course="Entree", restaurant=restaurant3)
 
-session.add(menuItem2)
+session.add(menuItem5)
 session.commit()
 
 
 # Menu for Thyme for that
-restaurant1 = Restaurant(name="Thyme for That Vegetarian Cuisine ")
+restaurant4 = Restaurant(name="Thyme for That Vegetarian Cuisine ")
 
-session.add(restaurant1)
+session.add(restaurant4)
 session.commit()
 
 
 menuItem1 = MenuItem(name="Tres Leches Cake", description="Rich, luscious sponge cake soaked in sweet milk and topped with vanilla bean whipped cream and strawberries.",
-                     price="$2.99", course="Dessert", restaurant=restaurant1)
+                     price="$2.99", course="Dessert", restaurant=restaurant4)
 
 session.add(menuItem1)
 session.commit()
 
 menuItem2 = MenuItem(name="Mushroom risotto", description="Portabello mushrooms in a creamy risotto",
-                     price="$5.99", course="Entree", restaurant=restaurant1)
+                     price="$5.99", course="Entree", restaurant=restaurant4)
 
 session.add(menuItem2)
 session.commit()
 
 menuItem3 = MenuItem(name="Honey Boba Shaved Snow", description="Milk snow layered with honey boba, jasmine tea jelly, grass jelly, caramel, cream, and freshly made mochi",
-                     price="$4.50", course="Dessert", restaurant=restaurant1)
+                     price="$4.50", course="Dessert", restaurant=restaurant4)
 
 session.add(menuItem3)
 session.commit()
 
 menuItem4 = MenuItem(name="Cauliflower Manchurian", description="Golden fried cauliflower florets in a midly spiced soya,garlic sauce cooked with fresh cilantro, celery, chilies,ginger & green onions",
-                     price="$6.95", course="Appetizer", restaurant=restaurant1)
+                     price="$6.95", course="Appetizer", restaurant=restaurant4)
 
 session.add(menuItem4)
 session.commit()
 
 menuItem5 = MenuItem(name="Aloo Gobi Burrito", description="Vegan goodness. Burrito filled with rice, garbanzo beans, curry sauce, potatoes (aloo), fried cauliflower (gobi) and chutney. Nom Nom",
-                     price="$7.95", course="Entree", restaurant=restaurant1)
+                     price="$7.95", course="Entree", restaurant=restaurant4)
 
 session.add(menuItem5)
 session.commit()
 
-menuItem2 = MenuItem(name="Veggie Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
-                     price="$6.80", course="Entree", restaurant=restaurant1)
+menuItem6 = MenuItem(name="Veggie Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
+                     price="$6.80", course="Entree", restaurant=restaurant4)
 
-session.add(menuItem2)
+session.add(menuItem6)
 session.commit()
 
 
 # Menu for Tony's Bistro
-restaurant1 = Restaurant(name="Tony\'s Bistro ")
+restaurant5 = Restaurant(name="Tony\'s Bistro ")
 
-session.add(restaurant1)
+session.add(restaurant5)
 session.commit()
 
 
 menuItem1 = MenuItem(name="Shellfish Tower", description="Lobster, shrimp, sea snails, crawfish, stacked into a delicious tower",
-                     price="$13.95", course="Entree", restaurant=restaurant1)
+                     price="$13.95", course="Entree", restaurant=restaurant5)
 
 session.add(menuItem1)
 session.commit()
 
 menuItem2 = MenuItem(name="Chicken and Rice", description="Chicken... and rice",
-                     price="$4.95", course="Entree", restaurant=restaurant1)
+                     price="$4.95", course="Entree", restaurant=restaurant5)
 
 session.add(menuItem2)
 session.commit()
 
 menuItem3 = MenuItem(name="Mom's Spaghetti", description="Spaghetti with some incredible tomato sauce made by mom",
-                     price="$6.95", course="Entree", restaurant=restaurant1)
+                     price="$6.95", course="Entree", restaurant=restaurant5)
 
 session.add(menuItem3)
 session.commit()
 
 menuItem4 = MenuItem(name="Choc Full O\' Mint (Smitten\'s Fresh Mint Chip ice cream)",
-                     description="Milk, cream, salt, ..., Liquid nitrogen magic", price="$3.95", course="Dessert", restaurant=restaurant1)
+                     description="Milk, cream, salt, ..., Liquid nitrogen magic", price="$3.95", course="Dessert", restaurant=restaurant5)
 
 session.add(menuItem4)
 session.commit()
 
 menuItem5 = MenuItem(name="Tonkatsu Ramen", description="Noodles in a delicious pork-based broth with a soft-boiled egg",
-                     price="$7.95", course="Entree", restaurant=restaurant1)
+                     price="$7.95", course="Entree", restaurant=restaurant5)
 
 session.add(menuItem5)
 session.commit()
 
 
 # Menu for Andala's
-restaurant1 = Restaurant(name="Andala\'s")
+restaurant6 = Restaurant(name="Andala\'s")
 
-session.add(restaurant1)
+session.add(restaurant6)
 session.commit()
 
 
 menuItem1 = MenuItem(name="Lamb Curry", description="Slow cook that thang in a pool of tomatoes, onions and alllll those tasty Indian spices. Mmmm.",
-                     price="$9.95", course="Entree", restaurant=restaurant1)
+                     price="$9.95", course="Entree", restaurant=restaurant6)
 
 session.add(menuItem1)
 session.commit()
 
 menuItem2 = MenuItem(name="Chicken Marsala", description="Chicken cooked in Marsala wine sauce with mushrooms",
-                     price="$7.95", course="Entree", restaurant=restaurant1)
+                     price="$7.95", course="Entree", restaurant=restaurant6)
 
 session.add(menuItem2)
 session.commit()
 
 menuItem3 = MenuItem(name="Potstickers", description="Delicious chicken and veggies encapsulated in fried dough.",
-                     price="$6.50", course="Appetizer", restaurant=restaurant1)
+                     price="$6.50", course="Appetizer", restaurant=restaurant6)
 
 session.add(menuItem3)
 session.commit()
 
 menuItem4 = MenuItem(name="Nigiri Sampler", description="Maguro, Sake, Hamachi, Unagi, Uni, TORO!",
-                     price="$6.75", course="Appetizer", restaurant=restaurant1)
+                     price="$6.75", course="Appetizer", restaurant=restaurant6)
 
 session.add(menuItem4)
 session.commit()
 
-menuItem2 = MenuItem(name="Veggie Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
-                     price="$7.00", course="Entree", restaurant=restaurant1)
+menuItem5 = MenuItem(name="Veggie Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
+                     price="$7.00", course="Entree", restaurant=restaurant6)
 
-session.add(menuItem2)
+session.add(menuItem5)
 session.commit()
 
 
 # Menu for Auntie Ann's
-restaurant1 = Restaurant(name="Auntie Ann\'s Diner ")
+restaurant7 = Restaurant(name="Auntie Ann\'s Diner ")
 
-session.add(restaurant1)
+session.add(restaurant7)
 session.commit()
 
-menuItem9 = MenuItem(name="Chicken Fried Steak", description="Fresh battered sirloin steak fried and smothered with cream gravy",
-                     price="$8.99", course="Entree", restaurant=restaurant1)
-
-session.add(menuItem9)
-session.commit()
-
-
-menuItem1 = MenuItem(name="Boysenberry Sorbet", description="An unsettlingly huge amount of ripe berries turned into frozen (and seedless) awesomeness",
-                     price="$2.99", course="Dessert", restaurant=restaurant1)
+menuItem1 = MenuItem(name="Chicken Fried Steak", description="Fresh battered sirloin steak fried and smothered with cream gravy",
+                     price="$8.99", course="Entree", restaurant=restaurant7)
 
 session.add(menuItem1)
 session.commit()
 
-menuItem2 = MenuItem(name="Broiled salmon", description="Salmon fillet marinated with fresh herbs and broiled hot & fast",
-                     price="$10.95", course="Entree", restaurant=restaurant1)
+
+menuItem2 = MenuItem(name="Boysenberry Sorbet", description="An unsettlingly huge amount of ripe berries turned into frozen (and seedless) awesomeness",
+                     price="$2.99", course="Dessert", restaurant=restaurant7)
 
 session.add(menuItem2)
 session.commit()
 
-menuItem3 = MenuItem(name="Morels on toast (seasonal)", description="Wild morel mushrooms fried in butter, served on herbed toast slices",
-                     price="$7.50", course="Appetizer", restaurant=restaurant1)
+menuItem3 = MenuItem(name="Broiled salmon", description="Salmon fillet marinated with fresh herbs and broiled hot & fast",
+                     price="$10.95", course="Entree", restaurant=restaurant7)
 
 session.add(menuItem3)
 session.commit()
 
-menuItem4 = MenuItem(name="Tandoori Chicken", description="Chicken marinated in yoghurt and seasoned with a spicy mix(chilli, tamarind among others) and slow cooked in a cylindrical clay or metal oven which gets its heat from burning charcoal.",
-                     price="$8.95", course="Entree", restaurant=restaurant1)
+menuItem4 = MenuItem(name="Morels on toast (seasonal)", description="Wild morel mushrooms fried in butter, served on herbed toast slices",
+                     price="$7.50", course="Appetizer", restaurant=restaurant7)
 
 session.add(menuItem4)
 session.commit()
 
-menuItem2 = MenuItem(name="Veggie Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
-                     price="$9.50", course="Entree", restaurant=restaurant1)
+menuItem5 = MenuItem(name="Tandoori Chicken", description="Chicken marinated in yoghurt and seasoned with a spicy mix(chilli, tamarind among others) and slow cooked in a cylindrical clay or metal oven which gets its heat from burning charcoal.",
+                     price="$8.95", course="Entree", restaurant=restaurant7)
 
-session.add(menuItem2)
+session.add(menuItem5)
 session.commit()
 
-menuItem10 = MenuItem(name="Spinach Ice Cream", description="vanilla ice cream made with organic spinach leaves",
-                      price="$1.99", course="Dessert", restaurant=restaurant1)
+menuItem6 = MenuItem(name="Veggie Burger", description="Juicy grilled veggie patty with tomato mayo and lettuce",
+                     price="$9.50", course="Entree", restaurant=restaurant7)
 
-session.add(menuItem10)
+session.add(menuItem6)
+session.commit()
+
+menuItem7 = MenuItem(name="Spinach Ice Cream", description="vanilla ice cream made with organic spinach leaves",
+                      price="$1.99", course="Dessert", restaurant=restaurant7)
+
+session.add(menuItem7)
 session.commit()
 
 
 # Menu for Cocina Y Amor
-restaurant1 = Restaurant(name="Cocina Y Amor ")
+restaurant8 = Restaurant(name="Cocina Y Amor ")
 
-session.add(restaurant1)
+session.add(restaurant8)
 session.commit()
 
 
 menuItem1 = MenuItem(name="Super Burrito Al Pastor", description="Marinated Pork, Rice, Beans, Avocado, Cilantro, Salsa, Tortilla",
-                     price="$5.95", course="Entree", restaurant=restaurant1)
+                     price="$5.95", course="Entree", restaurant=restaurant8)
 
 session.add(menuItem1)
 session.commit()
 
 menuItem2 = MenuItem(name="Cachapa", description="Golden brown, corn-based Venezuelan pancake; usually stuffed with queso telita or queso de mano, and possibly lechon. ",
-                     price="$7.99", course="Entree", restaurant=restaurant1)
+                     price="$7.99", course="Entree", restaurant=restaurant8)
 
 session.add(menuItem2)
 session.commit()
 
 
-restaurant1 = Restaurant(name="State Bird Provisions")
-session.add(restaurant1)
+restaurant9 = Restaurant(name="State Bird Provisions")
+session.add(restaurant9)
 session.commit()
 
 menuItem1 = MenuItem(name="Chantrelle Toast", description="Crispy Toast with Sesame Seeds slathered with buttery chantrelle mushrooms",
-                     price="$5.95", course="Appetizer", restaurant=restaurant1)
+                     price="$5.95", course="Appetizer", restaurant=restaurant9)
 
 session.add(menuItem1)
 session.commit()
 
-menuItem1 = MenuItem(name="Guanciale Chawanmushi", description="Japanese egg custard served hot with spicey Italian Pork Jowl (guanciale)",
-                     price="$6.95", course="Dessert", restaurant=restaurant1)
+menuItem2 = MenuItem(name="Guanciale Chawanmushi", description="Japanese egg custard served hot with spicey Italian Pork Jowl (guanciale)",
+                     price="$6.95", course="Dessert", restaurant=restaurant9)
 
-session.add(menuItem1)
+session.add(menuItem2)
 session.commit()
 
 
-menuItem1 = MenuItem(name="Lemon Curd Ice Cream Sandwich", description="Lemon Curd Ice Cream Sandwich on a chocolate macaron with cardamom meringue and cashews",
-                     price="$4.25", course="Dessert", restaurant=restaurant1)
+menuItem3 = MenuItem(name="Lemon Curd Ice Cream Sandwich", description="Lemon Curd Ice Cream Sandwich on a chocolate macaron with cardamom meringue and cashews",
+                     price="$4.25", course="Dessert", restaurant=restaurant9)
 
-session.add(menuItem1)
+session.add(menuItem3)
 session.commit()
 
 
