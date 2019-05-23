93b990e3c3a1b9346ddc625ea4c49ade0610d53c
Pritesh Shrivastava <pritesh2312@gmail.com>
2019-05-23 19:19:32 +0530

    [PS] Updated code to Python 3

--- Lesson-2/post-web-server/webserver.py
+++ Lesson-2/post-web-server/webserver.py
@@ -1,6 +1,7 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import cgi
-
+import sys
+## Source : https://stackoverflow.com/questions/36909047/do-post-not-working-on-http-server-with-python
 
 class webServerHandler(BaseHTTPRequestHandler):
 
@@ -15,8 +16,8 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output += "<h1>Hello!</h1>"
                 output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
-                self.wfile.write(output)
-                print output
+                self.wfile.write(output.encode())
+                print(output)
                 return
 
             if self.path.endswith("/hola"):
@@ -28,8 +29,8 @@ class webServerHandler(BaseHTTPRequestHandler):
                 output += "<h1>&#161 Hola !</h1>"
                 output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
                 output += "</body></html>"
-                self.wfile.write(output)
-                print output
+                self.wfile.write(output.encode())
+                print(output)
                 return
 
         except IOError:
@@ -41,30 +42,33 @@ class webServerHandler(BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
+                self.headers.get('content-type'))   ## Changed getheader to get
+            pdict['boundary'] = bytes(pdict['boundary'], "utf-8")    ## For Python 3
             if ctype == 'multipart/form-data':
                 fields = cgi.parse_multipart(self.rfile, pdict)
                 messagecontent = fields.get('message')
             output = ""
             output += "<html><body>"
             output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
+            output += "<h1> %s </h1>" % messagecontent[0].decode("utf-8")
             output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
             output += "</body></html>"
-            self.wfile.write(output)
-            print output
+            #self.wfile.write(bytes(output, "utf-8"))
+            self.wfile.write(output.encode())
+            print(output)
         except:
-            pass
+            self.send_error(404, "{}".format(sys.exc_info()[0]))
+            print(sys.exc_info())
 
 
 def main():
     try:
         port = 8080
         server = HTTPServer(('', port), webServerHandler)
-        print "Web Server running on port %s" % port
+        print("Web Server running on port %s" % port)
         server.serve_forever()
     except KeyboardInterrupt:
-        print " ^C entered, stopping web server...."
+        print(" ^C entered, stopping web server....")
         server.socket.close()
 
 if __name__ == '__main__':
