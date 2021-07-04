c71ba687e193216f3d7193a92d7dfe11bcda5e37
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-04 10:43:21 -0500

    edited print statements to  instead of  to comply with PEP 3105 --> https://www.python.org/dev/peps/pep-3105/

--- Lesson-2/first-web-server/webserver.py
+++ Lesson-2/first-web-server/webserver.py
@@ -11,7 +11,7 @@ class WebServerHandler(BaseHTTPRequestHandler):
             message = ""
             message += "<html><body>Hello!</body></html>"
             self.wfile.write(message)
-            print message
+            print(message)
             return
         else:
             self.send_error(404, 'File Not Found: %s' % self.path)
@@ -21,10 +21,10 @@ def main():
     try:
         port = 8080
         server = HTTPServer(('', port), WebServerHandler)
-        print "Web Server running on port %s" % port
+        print("Web Server running on port %s" % port)
         server.serve_forever()
     except KeyboardInterrupt:
-        print " ^C entered, stopping web server...."
+        print(" ^C entered, stopping web server....")
         server.socket.close()
 
 if __name__ == '__main__':
--- Lesson-2/hola-server/webserver.py
+++ Lesson-2/hola-server/webserver.py
@@ -11,7 +11,7 @@ class webServerHandler(BaseHTTPRequestHandler):
             message = ""
             message += "<html><body>Hello!</body></html>"
             self.wfile.write(message)
-            print message
+            print(message)
             return
 
         if self.path.endswith("/hola"):
@@ -21,7 +21,7 @@ class webServerHandler(BaseHTTPRequestHandler):
             message = ""
             message += "<html><body> &#161 Hola ! </body></html>"
             self.wfile.write(message)
-            print message
+            print(message)
             return
 
         else:
@@ -32,10 +32,10 @@ def main():
     try:
         port = 8080
         server = HTTPServer(('', port), webServerHandler)
-        print "Web Server running on port %s" % port
+        print("Web Server running on port %s" % port)
         server.serve_forever()
     except KeyboardInterrupt:
-        print " ^C entered, stopping web server...."
+        print(" ^C entered, stopping web server....")
         server.socket.close()
 
 if __name__ == '__main__':
